d81819c3f45f77663b45d4b314377e735d91a447
codedeman <kienluproductions@gmail.com>
2022-03-05 09:28:34 +0700

    new view component

--- SDUI/Models/ProductModels.swift
+++ SDUI/Models/ProductModels.swift
@@ -10,15 +10,33 @@ import Foundation
 
 struct ProductModel:Codable {
     var version: String?
-    var component:[Component]
+    var component:[ProductComponent]
 }
 
-struct Component:Codable,Hashable {
+struct ProductComponent:Codable {
+    var type:String?
+    var action:ActionModel?
+    var data:[ProductItemModel]
+}
+
+struct ProductItemModel:Codable,Hashable {
     var image_url:String?
     var price:String?
     var name:String?
+    var id:String?
 }
 
 struct ActionModel:Codable,Hashable {
     var autoScroll:Int?
 }
+
+
+//"type":"NewProduct",
+//"action":{"auto_scroll":1},
+//"data":[
+//    {
+//        "id":"134"
+//        "image_url":"https://doanhnhanplus.vn/wp-content/uploads/2021/12/Hyundai-Santa-Fe-2022-phien-ban-6-cho-20211213-dnplus-01.jpg"
+//        "name":"SantaFe"
+//        "price":"100000"
+//    },
--- SDUI/UIComponent/NewHeaderUIComponent.swift
+++ SDUI/UIComponent/NewHeaderUIComponent.swift
@@ -10,12 +10,26 @@ import Kingfisher
 import SwiftUI
 
 class NewHeaderUIComponent:UIComponent {
+   
+    
     var uniqueId: String
-    var component:ProductModel?
+    var component:ProductModel
     
     init(component:ProductModel) {
         self.uniqueId = "Nothing"
         self.component = component
+    }
+    
+    func getProductComponent() -> [ProductComponent] {
+        let arrComp = component.component.filter {$0.type  == "NewProduct"}
+
+        return arrComp
+    }
+//    
+    func render(uiDelegate: UIDelegate) -> AnyView {
+        let arrComp:[ProductComponent] = component.component.filter {$0.type  == "NewProduct"}
+
+        return NewHeaderView(component: arrComp).toAny()
     }
 //    init(tvShowResult:TvShowsResult) {
 //        self.uniqueId = tvShowResult.title ?? "Shows"
@@ -23,22 +37,37 @@ class NewHeaderUIComponent:UIComponent {
 //    }
 //    return TVShowView(tvShowResult: tvShowResult).toAny()
 
-    func render(uiDelegate: UIDelegate) -> AnyView {
-        return NewHeaderView(product: component).toAny()
-    }
+//    func render(uiDelegate: UIDelegate) -> AnyView {
+//        return NewHeaderView(
+//    }
     
     
 }
 
 struct NewHeaderView:View {
     
-    let product:Component?
-    
+    let component:ProductComponent
     var body: some View {
         NavigationView{
-            List(){
+            HStack {
+                ScrollView(.horizontal) {
+                    ForEach(component.data, id: \.id) { product    in
+                        NewFeedItem(item: product)
+                    }
+                }
                 
             }
+
         }.navigationBarTitle("New Product")
+    }
+}
+
+struct NewFeedItem:View {
+    let item:ProductItemModel
+    var body:some View {
+        VStack {
+            KFImage(URL(string: item.image_url ?? "")).resizable()
+            Text(item.name ?? "")
+        }
     }
 }
